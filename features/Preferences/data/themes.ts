c1c1a70b70b653f3b4f69c03237d764bae34c284
JeraldPascual <taofetch55@gmail.com>
2026-01-22 21:15:11 +0800

    feat(theme): add Pagoda Twilight theme

--- features/Preferences/data/themes.ts
+++ features/Preferences/data/themes.ts
@@ -253,11 +253,17 @@ const baseThemeSets: BaseThemeGroup[] = [
     icon: Moon,
     themes: [
       {
-  id: 'shaved-ice',
-  backgroundColor: 'oklch(95.0% 0.025 215.0 / 1)',
-  mainColor: 'oklch(60.0% 0.195 25.0 / 1)',
-  secondaryColor: 'oklch(65.0% 0.175 215.0 / 1)'
-},
+        id: 'shaved-ice',
+        backgroundColor: 'oklch(95.0% 0.025 215.0 / 1)',
+        mainColor: 'oklch(60.0% 0.195 25.0 / 1)',
+        secondaryColor: 'oklch(65.0% 0.175 215.0 / 1)'
+      },
+      {
+        id: 'pagoda-twilight',
+        backgroundColor: 'oklch(20.0% 0.045 290.0 / 1)',
+        mainColor: 'oklch(68.0% 0.155 315.0 / 1)',
+        secondaryColor: 'oklch(75.0% 0.125 280.0 / 1)'
+      },
       {
         id: 'lucky-bamboo',
         backgroundColor: 'oklch(92.0% 0.025 145.0 / 1)',
